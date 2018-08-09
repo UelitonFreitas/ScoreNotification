5d3ddd6209e0d0ed95a377ea1e6c18e193a46071
ueliton.freitas <ueliton.freitas@ueliton.local>
2018-08-09 15:38:40 -0300

    chore(project): run with OpenCV

--- app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
+++ app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
@@ -2,16 +2,35 @@ package com.notification.score.scorenotification
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.core.Mat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val camera = object :  CameraBridgeViewBase.CvCameraViewListener {
+        override fun onCameraViewStarted(width: Int, height: Int) {
+        }
+
+        override fun onCameraViewStopped() {
+        }
+
+        override fun onCameraFrame(inputFrame: Mat?): Mat {
+            Log.d("@@@", "frame")
+            return inputFrame!!
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val mOpenCvCameraView = findViewById<CameraBridgeViewBase>(R.id.live_camera_frame)
+        mOpenCvCameraView.setCvCameraViewListener(camera)
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+//        sample_text.text = stringFromJNI()
     }
 
     /**
