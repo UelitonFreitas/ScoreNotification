8e775f95bacee876253c15a56aaf52b1463c4edf
Fernando Pinho <fpinho@gmail.com>
2018-08-09 16:47:02 -0300

    add exoplayer

--- app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
+++ app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.notification.score.scorenotification
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import org.opencv.android.CameraBridgeViewBase
 import org.opencv.core.Mat
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit private var playerView : PlayerView
 
     private val camera = object :  CameraBridgeViewBase.CvCameraViewListener {
         override fun onCameraViewStarted(width: Int, height: Int) {
@@ -26,6 +37,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        playerView = findViewById(R.id.player_view)
+
+        val player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+        playerView.player = player
+        val mediaSource = ExtractorMediaSource.Factory(DefaultDataSourceFactory(this, Util.getUserAgent(this, "ScoreNotification"))).createMediaSource(Uri.parse("asset:///highline.mp4"));
+        player.prepare(mediaSource)
 
         val mOpenCvCameraView = findViewById<CameraBridgeViewBase>(R.id.live_camera_frame)
         mOpenCvCameraView.setCvCameraViewListener(camera)
