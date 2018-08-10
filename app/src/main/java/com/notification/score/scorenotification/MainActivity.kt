00cc7938fe808b05da3a6b06a55934dc215de5c9
ueliton.freitas <ueliton.freitas@ueliton.local>
2018-08-10 16:09:06 -0300

    hackday: GOGOGOGOGOGOGOGO!!!!!!!

--- app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
+++ app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
@@ -1,54 +1,44 @@
 package com.notification.score.scorenotification
 
-import android.net.Uri
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.TextureView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
-import android.view.SurfaceView
+import org.jetbrains.anko.doAsync
 import org.opencv.android.BaseLoaderCallback
-import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
 import org.opencv.core.Mat
-import android.view.TextureView
-import android.view.View
-import android.widget.ImageView
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
 
 
 class MainActivity() : AppCompatActivity() {
 
-    private val camera = object :  CameraBridgeViewBase.CvCameraViewListener2 {
-        override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame?): Mat {
-            Log.d("@@@", "frame")
-            return inputFrame?.rgba()!!
-        }
-
-        override fun onCameraViewStarted(width: Int, height: Int) {
-        }
-
-        override fun onCameraViewStopped() {
-        }
-    }
-
-    lateinit private var playerView : PlayerView
-    lateinit private var imageView : ImageView
+    lateinit private var playerView: PlayerView
+    lateinit private var imageView: ImageView
+    val scores by lazy { findViewById<TextView>(R.id.text_view_score) }
+    lateinit var ocr: TessOCR
 
     private var handler = Handler()
     private val runnable = Runnable { getImagePeriodically() }
-
-    private var mOpenCvCameraView : CameraBridgeViewBase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,25 +46,19 @@ class MainActivity() : AppCompatActivity() {
 
         playerView = findViewById(R.id.player_view)
         imageView = findViewById(R.id.imageView)
+        ocr = TessOCR(this) {
+            runOnUiThread{
+                scores.text = it
+            }
+        }
 
         val player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
         playerView.player = player
-        val mediaSource = ExtractorMediaSource.Factory(DefaultDataSourceFactory(this, Util.getUserAgent(this, "ScoreNotification"))).createMediaSource(Uri.parse("asset:///Brasil_Mexico_2oT_360.mp4"));
+        val mediaSource = ExtractorMediaSource.Factory(DefaultDataSourceFactory(this, Util.getUserAgent(this, "ScoreNotification"))).createMediaSource(Uri.parse("asset:///Brasil_Mexico_2oT_720.mp4"));
         player.prepare(mediaSource)
 
         getImagePeriodically()
 
-        mOpenCvCameraView = findViewById(R.id.live_camera_frame)
-        mOpenCvCameraView?.visibility = SurfaceView.VISIBLE
-        mOpenCvCameraView?.setCvCameraViewListener(camera)
-
-        // Example of a call to a native method
-//        sample_text.text = stringFromJNI()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mOpenCvCameraView?.disableView()
     }
 
     private val cameraRequestCode = 100
@@ -82,10 +66,9 @@ class MainActivity() : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequestCode)
-        }
-        else{
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), cameraRequestCode)
+        } else {
             initOpenCv()
         }
     }
@@ -107,7 +90,6 @@ class MainActivity() : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        mOpenCvCameraView?.disableView()
         handler.removeCallbacks(runnable)
     }
 
@@ -116,7 +98,9 @@ class MainActivity() : AppCompatActivity() {
             when (status) {
                 LoaderCallbackInterface.SUCCESS -> {
                     Log.i("opencv", "opencv success")
-                    mOpenCvCameraView?.enableView()
+                    doAsync {
+                        ocr.initalizeTess()
+                    }
                 }
                 else -> {
                     super.onManagerConnected(status)
@@ -136,12 +120,36 @@ class MainActivity() : AppCompatActivity() {
 
     fun getImage() {
         val textureView = playerView.getVideoSurfaceView() as TextureView
-        val bitmap = textureView.getBitmap()
-        bitmap?.let {
-            Log.d("@@@", "Bitmap: ${bitmap?.height}/${bitmap?.width}")
-            val placar = Bitmap.createBitmap(it, 0, it.height/20, it.width/2, it.height/10)
-            imageView.setImageBitmap(placar)
+        val bitmap = textureView.bitmap
+
+        doAsync {
+            var text: String? = null
+            var placar : Bitmap? = null
+
+            bitmap?.let {
+                Log.d("@@@", "Bitmap: ${bitmap?.height}/${bitmap?.width}")
+                placar = Bitmap.createBitmap(it, 10, it.height / 20, it.width / 4, it.height / 10)
+
+                val img_mat = Mat()
+                Utils.bitmapToMat(placar, img_mat)
+
+                val img_gray = Mat()
+
+                Imgproc.cvtColor(img_mat, img_gray, Imgproc.COLOR_BGR2GRAY)
+
+                val img_blurred = Mat()
+
+                Imgproc.GaussianBlur(img_gray, img_blurred,  Size(3.0,3.0), 2.0)
+
+                Utils.matToBitmap(img_gray, placar)
+                ocr.processOcr(placar!!)
+            }
+
+            runOnUiThread {
+                imageView.setImageBitmap(placar)
+            }
         }
+//        imageView.setImageBitmap(bitmap)
     }
 
     /**
@@ -157,4 +165,8 @@ class MainActivity() : AppCompatActivity() {
             System.loadLibrary("native-lib")
         }
     }
+
+
 }
+
+
