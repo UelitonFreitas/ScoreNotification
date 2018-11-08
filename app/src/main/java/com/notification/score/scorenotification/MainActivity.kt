ec156029bff4ba82a22f5c1ca93ec534cc3f3699
uelitonfreitas <freitas.ueliton@gmail.com>
2018-11-08 16:13:58 -0200

    chore(opencv): remove OpenCV

--- app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
+++ app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -21,12 +20,8 @@ import com.google.android.exoplayer2.util.Util
 import com.notification.score.scorenotification.classifiers.FirebaseVisionDocumentTextRecognizer
 import com.notification.score.scorenotification.classifiers.ScoreClassifier
 import com.notification.score.scorenotification.classifiers.ScoreClassifierImpl
-import com.notification.score.scorenotification.imageprocessor.OpenCvImageProcessor
 import com.notification.score.scorenotification.imageprovider.ImageProvider
 import com.notification.score.scorenotification.imageprovider.ImageProviderImpl
-import org.opencv.android.BaseLoaderCallback
-import org.opencv.android.LoaderCallbackInterface
-import org.opencv.android.OpenCVLoader
 
 
 class MainActivity() : AppCompatActivity() {
@@ -69,9 +64,7 @@ class MainActivity() : AppCompatActivity() {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), cameraRequestCode)
-        } else {
-            initOpenCv()
-        }
+        } else { }
 
         imageProvider = ImageProviderImpl(playerView)
         scoreClassifier = ScoreClassifierImpl(this).apply { start() }
@@ -85,39 +78,11 @@ class MainActivity() : AppCompatActivity() {
         runOnUiThread { imageView.setImageBitmap(bitmap) }
     }
 
-    private fun initOpenCv() {
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback)
-    }
-
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == cameraRequestCode) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initOpenCv()
-            }
-        }
-    }
 
     override fun onDestroy() {
         scoreRecognizer.stopWatchScoreChange()
         super.onDestroy()
 
-    }
-
-    private val mLoaderCallback = object : BaseLoaderCallback(this) {
-        override fun onManagerConnected(status: Int) {
-            when (status) {
-                LoaderCallbackInterface.SUCCESS -> {
-                    Log.i("opencv", "opencv success")
-                    scoreRecognizer.imageProcessor = OpenCvImageProcessor()
-                }
-                else -> {
-                    super.onManagerConnected(status)
-                }
-            }
-        }
     }
 
     private fun onScoreFound(score: String) {
