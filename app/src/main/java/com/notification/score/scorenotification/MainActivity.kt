3c6d4b92a9d2b76eb812d78a7f00987c48536514
Fernando Pinho <fpinho@gmail.com>
2018-11-14 18:24:54 -0200

    feat(globo_player): add Globo Player

--- app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
+++ app/src/main/java/com/notification/score/scorenotification/MainActivity.kt
@@ -8,9 +8,15 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.TextureView
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.globo.video.player.Player
+import com.globo.video.player.PlayerOption
+import com.globo.video.player.base.PlayerMimeType
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
@@ -22,11 +28,14 @@ import com.notification.score.scorenotification.classifiers.ScoreClassifier
 import com.notification.score.scorenotification.classifiers.ScoreClassifierImpl
 import com.notification.score.scorenotification.imageprovider.ImageProvider
 import com.notification.score.scorenotification.imageprovider.ImageProviderImpl
+import io.clappr.player.base.ClapprOption
+import io.clappr.player.base.Options
 
 
 class MainActivity() : AppCompatActivity() {
     private lateinit var globoPlayer : Player
     private lateinit var imageView: ImageView
+    private lateinit var playerContainer: ViewGroup
     private val scores: TextView by lazy { findViewById<TextView>(R.id.text_view_score) }
     lateinit var imageProvider: ImageProvider
     lateinit var scoreClassifier: ScoreClassifier
@@ -36,18 +45,26 @@ class MainActivity() : AppCompatActivity() {
     private val cameraRequestCode = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        io.clappr.player.Player.initialize(applicationContext)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playerView = findViewById(R.id.player_view)
         imageView = findViewById(R.id.imageView)
+        playerContainer = findViewById(R.id.player_container)
 
         scoreClassifier = FirebaseVisionDocumentTextRecognizer(this.applicationContext).apply { start() }
 
-        val player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
-        playerView.player = player
-        val mediaSource = ExtractorMediaSource.Factory(DefaultDataSourceFactory(this, Util.getUserAgent(this, "ScoreNotification"))).createMediaSource(Uri.parse("asset:///Brasil_Mexico_2oT_720.mp4"));
-        player.prepare(mediaSource)
+        globoPlayer = Player()
+
+        val options = hashMapOf<String, Any>(
+                PlayerOption.TOKEN.value to "10c2198d248307aef236cf5ba92a24cc751655a7a3445734f454542734c65694e6d6d7744596b664e6a2d35503570795733766139445846576b47506433715157546c55374a715a65375a51746571374b75396c4e684242756b6d5a537466755f6174784b73513d3d3a303a74657374653939392e76656c6f782e3035",
+                ClapprOption.START_AT.value to 280)
+        globoPlayer.configure(Options("6851541", PlayerMimeType.VIDEO_ID.value, options))
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.player_container, globoPlayer)
+        fragmentTransaction.commit()
     }
 
 
@@ -58,7 +75,7 @@ class MainActivity() : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), cameraRequestCode)
         } else { }
 
-        imageProvider = ImageProviderImpl(playerView)
+        imageProvider = ImageProviderImpl(globoPlayer)
         scoreClassifier = ScoreClassifierImpl(this).apply { start() }
         scoreRecognizer = ScoreRecognizer(imageProvider, scoreClassifier, ::onScoreFound).apply {
             onImageProcessed = ::onImageProcessed
@@ -83,6 +100,9 @@ class MainActivity() : AppCompatActivity() {
 
     fun buttonClick(v: View) {
         scoreRecognizer.findScore()
+        val playerBitmap = globoPlayer.getVideoFrame()
+        val videoBitmap = ((globoPlayer.core?.activePlayback?.view as? PlayerView)?.videoSurfaceView as? TextureView)?.bitmap
+        Log.d("Scorenotification", "onImageProcessed: (${playerBitmap?.width}/${playerBitmap?.height}) / (${videoBitmap?.width}/${videoBitmap?.height})")
     }
 }
 
